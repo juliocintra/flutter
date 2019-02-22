d9512f30bcea9e77903f5cd71648e3db955a9610
Julio Cesar <julinn_cesar@hotmail.com>
2019-02-22 01:21:33 -0300

    iniciado conversor de moedas

---
+++ conversor/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+
+const request = 'https://api.hgbrasil.com/finance?key==82598356';
+
+void main() async{
+
+  http.Response  response = await http.get(request);
+  print(response.body);
+
+  runApp(MaterialApp(home: Container()));
+}
