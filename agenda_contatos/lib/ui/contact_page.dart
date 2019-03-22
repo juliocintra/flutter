dfe533d918bd562861235091c95dc91472369290
unknown <julio.cesar@smn.com.br>
2019-03-22 08:44:42 -0300

    --

--- agenda_contatos/lib/ui/contact_page.dart
+++ agenda_contatos/lib/ui/contact_page.dart
@@ -40,79 +40,113 @@ class _ContactPageState extends State<ContactPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: Text(_editedContact.name ?? 'Novo Contato'),
-          centerTitle: true,
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            if (_editedContact.name != null && _editedContact.name.isNotEmpty) {
-              Navigator.pop(context, _editedContact);           // remove a tela e volta pra anterior
-            } else {
-              FocusScope.of(context).requestFocus(_nameFocus);  // da o foco no campo não preenchido
-            }
-          },
-          child: Icon(Icons.save),
-          backgroundColor: Colors.red,
-        ),
-        body: SingleChildScrollView(
-          padding: EdgeInsets.all(10.0),
-          child: Column(
-            children: <Widget>[
-              GestureDetector(
-                child: Container(
-                  width: 140.0,
-                  height: 140.0,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                          image: _editedContact.img != null
-                              ? FileImage(File(_editedContact.img))
-                              : AssetImage('images/catra.png'))),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 10.0),
-                child: TextField(
-                  controller: _nameController,
-                  focusNode: _nameFocus,
-                  decoration: InputDecoration(labelText: 'Nome'),
-                  onChanged: (text) {
-                    _userEdited = true;
-                    setState(() {
-                      _editedContact.name = text;
-                    });
-                  },
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 10.0),
-                child: TextField(
-                  controller: _emailController,
-                  decoration: InputDecoration(labelText: 'Email'),
-                  onChanged: (text) {
-                    _userEdited = true;
-                    _editedContact.email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 10.0),
-                child: TextField(
-                  controller: _phoneController,
-                  decoration: InputDecoration(labelText: 'Phone'),
-                  onChanged: (text) {
-                    _userEdited = true;
-                    _editedContact.phone = text;
-                  },
-                  keyboardType: TextInputType.phone,
-                ),
-              )
-            ],
+    return WillPopScope(
+      onWillPop: _requestPop,
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.red,
+            title: Text(_editedContact.name ?? 'Novo Contato'),
+            centerTitle: true,
           ),
-        ));
+          floatingActionButton: FloatingActionButton(
+            onPressed: (){
+              if (_editedContact.name != null && _editedContact.name.isNotEmpty) {
+                Navigator.pop(context, _editedContact);           // remove a tela e volta pra anterior
+              } else {
+                FocusScope.of(context).requestFocus(_nameFocus);  // da o foco no campo não preenchido
+              }
+            },
+            child: Icon(Icons.save),
+            backgroundColor: Colors.red,
+          ),
+          body: SingleChildScrollView(
+            padding: EdgeInsets.all(10.0),
+            child: Column(
+              children: <Widget>[
+                GestureDetector(
+                  child: Container(
+                    width: 140.0,
+                    height: 140.0,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            image: _editedContact.img != null
+                                ? FileImage(File(_editedContact.img))
+                                : AssetImage('images/catra.png'))),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: TextField(
+                    controller: _nameController,
+                    focusNode: _nameFocus,
+                    decoration: InputDecoration(labelText: 'Nome'),
+                    onChanged: (text) {
+                      _userEdited = true;
+                      setState(() {
+                        _editedContact.name = text;
+                      });
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: TextField(
+                    controller: _emailController,
+                    decoration: InputDecoration(labelText: 'Email'),
+                    onChanged: (text) {
+                      _userEdited = true;
+                      _editedContact.email = text;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: TextField(
+                    controller: _phoneController,
+                    decoration: InputDecoration(labelText: 'Phone'),
+                    onChanged: (text) {
+                      _userEdited = true;
+                      _editedContact.phone = text;
+                    },
+                    keyboardType: TextInputType.phone,
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
+  }
+
+  Future<bool> _requestPop() {
+    if (_userEdited) {
+      showDialog(context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text('Descartar alterações?'),
+              content: Text('Se sair as alterações serão perdidadas.'),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('Cancelar'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                FlatButton(
+                  child: Text('Sim'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                    Navigator.pop(context);
+                  },
+                )
+              ],
+            );
+          }
+      );
+      return Future.value(false);
+    } else {
+      return Future.value(true); // retorna automaticamente, sem verificar se existe dados não salvos
+    }
   }
 }
